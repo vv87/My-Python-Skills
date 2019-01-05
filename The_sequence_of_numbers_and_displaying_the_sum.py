dc14f0294d8d40b9724b600cbd58f5aba5834764
vv87 <38062151+vv87@users.noreply.github.com>
2019-01-05 17:21:46 +0300

    Create The_sequence_of_numbers_and_displaying_the_sum.py

---
+++ The_sequence_of_numbers_and_displaying_the_sum.py
@@ -0,0 +1,80 @@
+n = int(input())
+s = 0
+for i in range(n):
+    n = int(input())
+    s += n 
+print(s)
+
+'''
+Implement a program that accepts a sequence of numbers and displays their sum.
+
+ Your program is given a sequence of lines.
+ The first line contains the number n (1 ≤ n ≤ 100).
+ The next n lines contain one integer.
+
+ Print a single number - the sum of the given n numbers.
+
+ Note:
+ To read one number from the standard input stream, you can use, for example, the following code
+
+ n = int (input ())
+ Sample Input 1:
+
+ 2
+ 2
+ 3
+ Sample Output 1:
+
+ five
+ Sample Input 2:
+
+ 2
+ -2
+ -2
+ Sample Output 2:
+
+ -four
+ Sample Input 3:
+
+ one
+ 31
+ Sample Output 3:
+
+ 31
+ 
+ 
+ Реализуйте программу, которая принимает последовательность чисел и выводит их сумму.
+
+Вашей программе на вход подается последовательность строк.
+Первая строка содержит число n (1 ≤ n ≤ 100).
+В следующих n строках содержится по одному целому числу.
+
+Выведите одно число – сумму данных n чисел.
+
+Примечание:
+Чтобы считать одно число из стандартного потока ввода, можно использовать, например, следующий код
+
+n = int(input())
+Sample Input 1:
+
+2
+2
+3
+Sample Output 1:
+
+5
+Sample Input 2:
+
+2
+-2
+-2
+Sample Output 2:
+
+-4
+Sample Input 3:
+
+1
+31
+Sample Output 3:
+
+31'''
